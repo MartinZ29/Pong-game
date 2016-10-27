de5cad174bc1aacaa41a1bfe94a4d6dc67b95914
martinz <martinzhangyx@gmail.com>
2016-10-27 10:41:42 -0700

    add game class

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ import './game.css';
 class Game {
 	constructor() {
 		const canvas = document.getElementById('game');
+		this.width = canvas.width;
+		this.height = canvas.height;
+		this.context = canvas.getContext('2d');
+		this.context.fillStyle = 'white';
 	}
 }
+
+var game = new Game();
+
+const ms = 30;
+(function gameLoop(){
+	console.log(game.height,game.width)
+	setTimeout(gameLoop,ms);
+}());
