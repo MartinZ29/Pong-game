bf31204d43892b79c7e8dcbba67ade4183cd98c3
martinz <martinzhangyx@gmail.com>
2016-10-27 11:01:54 -0700

    drawLine

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ class Game {
 		this.context = canvas.getContext('2d');
 		this.context.fillStyle = 'white';
 	}
+
+	drawLine(){
+		this.context.setLineDash([10,10]);
+		this.context.beginPath();
+		this.context.moveTo(this.width/2,0);
+		this.context.lineTo(this.width/2,this.height);
+		this.context.strokeStyle = 'white';
+		this.context.stroke();
+	}
 }
 
 var game = new Game();
@@ -15,5 +24,10 @@ var game = new Game();
 const ms = 30;
 (function gameLoop(){
 	console.log(game.height,game.width)
+	setTimeout(gameLoop,ms);
+}());
+
+(function gameLoop(){
+	game.drawLine();
 	setTimeout(gameLoop,ms);
 }());
